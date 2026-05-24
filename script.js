00376afc47799e5e2cc1c1f5094afaddf147521f
lynn0130tennis <linglingxu0502@gmail.com>
2026-05-24 18:03:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const SUPABASE_URL = "https://uppzqygxtpoifkaddoyi.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjYW9sdGZhbGhzdG52amhjeGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NjQzNTcsImV4cCI6MjA5NTE0MDM1N30.qqoegdtiXp5Xnfoa-TASwJT2S_InYfkJyfaAO9hviys";
+const SUPABASE_KEY = "sb_publishable_RXBO8ICJ7jGQFKYh2wDaig_7CqDU9fe";
 
 
 
