f4e4066b99b1f343fd7e0a9b30c28df547a8676e
lynn0130tennis <linglingxu0502@gmail.com>
2026-05-23 22:32:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("SCRIPT LOADED1")
 alert("script loaded2");
 
-const SUPABASE_URL = "https://wcaoltfalhstnvjhcxbi.supabase.co/rest/v1/";
-const SUPABASE_KEY = "sb_publishable_bFN87oygYFfa9i1fKREwug_7I96hoPb";
+const SUPABASE_URL = "https://wcaoltfalhstnvjhcxbi.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjYW9sdGZhbGhzdG52amhjeGJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NjQzNTcsImV4cCI6MjA5NTE0MDM1N30.qqoegdtiXp5Xnfoa-TASwJT2S_InYfkJyfaAO9hviys";
 
 
 
